9839e023286957206e30f75cd6bb74cf79528ec9
Cesar <cesarramez8@gmail.com>
2023-05-12 17:44:55 -0600

    Add application components to the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import TaskCounter from './components/TaskCounter'
+import TaskSearch from './components/TaskSearch'
+import TaskList from './components/TaskList'
+import TaskItem from './components/TaskItem'
+import TaskButton from './components/TaskButton'
+import './styles/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <TaskCounter />
+      <TaskSearch />
+      <TaskList>
+        <TaskItem />
+      </TaskList>
+      <TaskButton />
+    </div>
   )
 }
 
