9ac3237b99ba13d15d368c1e78f128863769e126
Cesar <cesarramez8@gmail.com>
2023-05-12 17:47:31 -0600

    Add fake task for the props of the TaskItem component

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,23 @@ import TaskItem from './components/TaskItem'
 import TaskButton from './components/TaskButton'
 import './styles/App.css'
 
+const fakeTask = [
+  { task: 'Ver curso de Vite', completed: true },
+  { task: 'Ver curso de React', completed: false },
+  { task: 'Ver curso de React: Patrones de render', completed: false }
+]
+
 function App () {
   return (
     <div className='App'>
       <TaskCounter />
       <TaskSearch />
       <TaskList>
-        <TaskItem />
+        {
+          fakeTask.map(({ task }) => (
+            <TaskItem key={task} task={task} />
+          ))
+        }
       </TaskList>
       <TaskButton />
     </div>
