ac4a6d44fea2d65e74ab993990e720902ca88b0b
Cesar <cesarramez8@gmail.com>
2023-05-15 18:20:15 -0600

    Add markCompleted function and state

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,30 @@ const fakeTask = [
   { task: 'Ver curso de React: Patrones de render', completed: false }
 ]
 
-const totalTask = fakeTask.length
-const completedTask = fakeTask.filter(task => task.completed === true).length
-
 function App () {
   const [search, setSearch] = useState('')
+  const [tasks, setTasks] = useState(fakeTask)
 
-  const searchList = fakeTask.filter(task => {
+  const searchList = tasks.filter(task => {
     const taskList = task.task.toLocaleLowerCase()
     return taskList.includes(search.toLocaleLowerCase())
   })
+
+  const markCompleted = (taskText) => {
+    const findTask = tasks.findIndex(task => task.task === taskText)
+    const copyTaskList = [...tasks]
+
+    if (copyTaskList[findTask].completed === true) {
+      copyTaskList[findTask].completed = false
+    } else {
+      copyTaskList[findTask].completed = true
+    }
+
+    setTasks(copyTaskList)
+  }
+
+  const totalTask = tasks.length
+  const completedTask = tasks.filter(task => task.completed === true).length
 
   return (
     <div className='App'>
@@ -29,8 +43,13 @@ function App () {
       <TaskSearch search={search} setSearch={setSearch} />
       <TaskList>
         {
-          searchList.map(({ task }) => (
-            <TaskItem key={task} task={task} />
+          searchList.map(({ task, completed }) => (
+            <TaskItem
+              key={task}
+              task={task}
+              completed={completed}
+              taskCompleted={() => markCompleted(task)}
+            />
           ))
         }
       </TaskList>
