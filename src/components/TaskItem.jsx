8ac6d78cf8dee5df36e26537717526a91735f487
Cesar <cesarramez8@gmail.com>
2023-05-15 18:17:07 -0600

    Add event and props to mark task completed

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { IconCircleCheck, IconTrash } from '@tabler/icons-react'
 import '../styles/TaskItem.css'
 
-export default function TaskItem ({ task }) {
+export default function TaskItem ({ task, completed, taskCompleted }) {
+  const markCompleted = completed ? 'Check-completed' : ''
+
   return (
     <li className='task-li'>
-      <span className='task-completedSpan'>
+      <span className={`task-completedSpan ${markCompleted}`} onClick={taskCompleted}>
         <IconCircleCheck className='task-completedIcon' />
       </span>
 
       <div className='task-containerText'>
-        <p className='task-text'>
+        <p className={`task-text ${markCompleted}`}>
           {
             task
           }
