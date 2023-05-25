c420ff4aebb12f64481ca44983c20da06737a178
Cesar <cesarramez8@gmail.com>
2023-05-24 19:36:01 -0600

    Move App component logic to the context file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,54 +1,13 @@
-import React, { useState } from 'react'
-import useLocalStorage from '../../Hooks/useLocalStorage/useLocalStorage'
+import React from 'react'
 import AppUI from './AppUI'
+import { TaskProvider } from '../../contexts/TaskProvider/TaskProvider'
 import './App.css'
 
 function App () {
-  // Se reciben y renombran (item y saveItem) los valores retornados del Custom Hook
-  const { item: tasks, saveItem: saveTasks, loading, error } = useLocalStorage('TASK_LIST_V1', [])
-  const [search, setSearch] = useState('')
-
-  const searchList = tasks.filter(task => {
-    const taskList = task.task.toLocaleLowerCase()
-    return taskList.includes(search.toLocaleLowerCase())
-  })
-
-  const markCompleted = (taskText) => {
-    const findTask = tasks.findIndex(task => task.task === taskText)
-    const copyTaskList = [...tasks]
-
-    if (copyTaskList[findTask].completed === true) {
-      copyTaskList[findTask].completed = false
-    } else {
-      copyTaskList[findTask].completed = true
-    }
-
-    saveTasks(copyTaskList)
-  }
-
-  const deleteTask = (taskText) => {
-    const findTask = tasks.findIndex(task => task.task === taskText)
-    const copyTaskList = [...tasks]
-    copyTaskList.splice(findTask, 1)
-
-    saveTasks(copyTaskList)
-  }
-
-  const totalTask = tasks.length
-  const completedTask = tasks.filter(task => task.completed === true).length
-
   return (
-    <AppUI
-      loading={loading}
-      error={error}
-      totalTask={totalTask}
-      completedTask={completedTask}
-      search={search}
-      setSearch={setSearch}
-      searchList={searchList}
-      markCompleted={markCompleted}
-      deleteTask={deleteTask}
-    />
+    <TaskProvider>
+      <AppUI />
+    </TaskProvider>
   )
 }
 
