61bb40c60c96fac5e8fecff20ae7f13bded4c256
Cesar <cesarramez8@gmail.com>
2023-05-25 20:24:46 -0600

    Add context data to the TaskCounter component

--- src/components/TaskCounter/TaskCounter.jsx
+++ src/components/TaskCounter/TaskCounter.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TaskContext } from '../../contexts/TaskProvider/TaskProvider'
 import './TaskCounter.css'
 
-export default function TaskCounter ({ completedTask, totalTask }) {
+export default function TaskCounter () {
+  const { completedTask, totalTask } = useContext(TaskContext)
+
   return (
     <h1 className='task-counter'>
       Haz completado {completedTask} de {totalTask} tareas.
