6f276fc9f15ff9fced73c873e53a96b188a2f1c6
Cesar <cesarramez8@gmail.com>
2023-05-30 12:26:19 -0600

    Add toggle function for modal

--- src/components/TaskButton/TaskButton.jsx
+++ src/components/TaskButton/TaskButton.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { IconPlus } from '@tabler/icons-react'
+import { TaskContext } from '../../contexts/TaskProvider/TaskProvider'
 import './TaskButton.css'
 
 export default function TaskButton () {
+  const { openModal, setOpenModal } = useContext(TaskContext)
+
+  const toggleModal = () => {
+    // !openModal alterna (hace un toggle) con el valor del estado.
+    setOpenModal(!openModal)
+  }
+
   return (
-    <button className='task-button'>
+    <button
+      className='task-button'
+      onClick={toggleModal}
+    >
       <IconPlus className='task-addIcon' />
     </button>
   )
