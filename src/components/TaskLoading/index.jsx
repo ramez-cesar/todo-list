de8109ce9780fdaf2f0c859dd647ea58fce04f5b
Cesar <cesarramez8@gmail.com>
2023-09-13 14:19:35 -0600

    Add TaskLoading component

---
+++ src/components/TaskLoading/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TaskLoading = () => {
+  return (
+    <p className='message msg-loading'>
+      <span className='loader' />
+      Cargando tareas...
+    </p>
+  )
+}
+
+export default TaskLoading
